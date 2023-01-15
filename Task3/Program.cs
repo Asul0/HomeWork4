db4bb4692d83c3a73590c5a16662af08a695c4c8
Volodya <volodya.atoyan@list.ru>
2023-01-15 19:14:30 +0300

    Added Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 29: Напишите программу, реализующую метод, который
+//формирует массив случайных целых чисел из 8 элементов и 
+//выводит их на экран.1,2,5,7,19,6,1,33 -> [1,2,5,7,19,6,1,33 ]
+
+
+using static System.Console;
+Clear();
+
+
+
+int[] sum = InitBinaryArray();
+PrintArray(sum);
+
+
+
+int[] InitBinaryArray()
+{
+    int[] res = new int[8];
+
+    for (int i = 0; i < res.Length; i++)
+    {
+        res[i] = new Random().Next(100);
+    }
+    return res;
+}
+
+void PrintArray(int[] arr)
+{
+    Write("[");
+    for (int i = 0; i < 7; i++)
+    {
+        Write($"{arr[i]},");
+    }
+    for (int i = 0; i < 1; i++)     //данный цикл, чтобы последний массив был без ","  
+    {
+        Write($"{arr[7]}");
+    }
+    Write("]");
+}
